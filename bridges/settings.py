565002a7cc83c8d24b0af32ec8da773b858bb313
Simon Wörpel <simon.woerpel@medienrevolte.de>
2017-08-10 16:16:26 +0200

    Update STATIC_ROOT

--- bridges/settings.py
+++ bridges/settings.py
@@ -97,7 +97,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "../static")
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
